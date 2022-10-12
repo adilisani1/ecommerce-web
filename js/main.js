5d559e603f60d1efd453a0d5a274545b719b4734
Adil Isani <adil.isani1@gmail.com>
2022-10-12 17:27:15 +0500

    ADD to cart button

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// Swiper
+var swiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 5000,
+    },
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 1,
+        },
+        991: {
+            slidesPerView: 1,
+        },
+    },
+});
